b18fa752941d4af94dfdc68c9ad89661e3fb9cdd
SIMHONGSUB <oxhe2038@naver.com>
2018-12-28 11:34:54 +0900

    baekjoon algorithm 2941 answer

---
+++ baekjoon/baekjoon_2941.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//c=, c-, dz=, d-, lj, nj, s=, z=
+int main(){
+
+	string input;
+	string alpha[8] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+	int result = 0, i;
+
+	cin >> input;
+
+	for (i = 0; i < 8; i++){
+
+		while (input.find(alpha[i]) != string::npos){
+			result++;
+			if (i == 2){
+				input.replace(input.find(alpha[i]), 3, "000");
+			}
+			else{
+				input.replace(input.find(alpha[i]), 2, "00");
+			}
+		}
+		/*if (input.find(alpha[i]) != string::npos){
+			result++;
+			if (i == 2){
+				input.replace(input.find(alpha[i]), 3, "000");
+			}
+			else{
+				input.replace(input.find(alpha[i]), 2, "00");
+			}
+		}*/
+	}
+
+	for (i = 0; i < input.size(); i++){
+		if (input.at(i) != '0'){
+			result++;
+		}
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
