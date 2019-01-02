ccbba09657da4bbf5c025ee4d072c0157565f0c4
SIMHONGSUB <oxhe2038@naver.com>
2019-01-02 16:13:12 +0900

    baekjoon algorithm 1011 answer

--- baekjoon/baekjoon_1011.cpp
+++ baekjoon/baekjoon_1011.cpp
@@ -6,20 +6,32 @@ using namespace std;
 int main(){
 
 	int numOfTest;
-	double x, y, distance;
+	int x, y, distance;
 	int k, temp, result;
 	
 	scanf("%d", &numOfTest);
 
 	for (int i = 0; i < numOfTest; i++){
-		scanf("%lf %lf", &x, &y);
+		scanf("%d %d", &x, &y);
 
 		distance = y - x;
-		k = sqrt(distance);
-		printf("%d\n", k);
-		result = 1;
+		k = sqrt((double)distance);
 
-		while (1){
+		if ((k*k) != distance){
+			k += 1;
+		}
+
+		result = 2 * k - 1;
+		temp = ((k*k) - ((k - 1)*(k - 1))) / 2;
+
+		if (temp < (k * k) - distance){
+			printf("%d\n", result - 1);
+		}
+		else{
+			printf("%d\n", result);
+		}
+		
+		/*while (1){
 			if (k*k >= distance){
 				if (temp < (k*k) - distance){
 					printf("%d\n", result - 1);
@@ -32,7 +44,7 @@ int main(){
 			k++;
 			temp = ((k*k) - ((k - 1)*(k - 1))) / 2;
 			result += 2;
-		}
+		}*/
 	}
 
 	return 0;
