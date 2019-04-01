b846dd0417e4c0ccd9a6d14c099b86efc289410c
SimHongSub <ghdtjq2038@gmail.com>
2019-04-01 10:44:45 +0900

    baekjoon algorithm 10989 answer

--- baekjoon/baekjoon_10989.cpp
+++ baekjoon/baekjoon_10989.cpp
@@ -1,51 +1,30 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 
 using namespace std;
 
-#define MAX_NUM 10000
-
 int main(){
 
-	int N;
-	int size;
+	int N, input;
+	int count[10001] = {0,};
 
-	cin >> N;
+	scanf("%d", &N);
 
-	int *input = new int[N + 1];
-	int *sortArr = new int[N + 1];
-
-	for (int i = 1; i <= N; i++){
-		cin >> input[i];
+	for (int i = 0; i < N; i++){
+		scanf("%d",&input);
+		count[input]++;
 	}
 
-
-	size = *max_element(input, input + N + 1);
-	int *count = new int[size + 1];
-	int *countSum = new int[size + 1];
-
-	memset(count, 0, (size+1)*sizeof(int));
-
-	for (int i = 1; i <= N; i++){
-		count[input[i]]++;
+	for (int i = 1; i < 10001; i++){
+		count[i] += count[i - 1];
 	}
 
-	countSum[0] = count[0];
-
-	for (int i = 1; i <= size; i++){
-		countSum[i] = countSum[i - 1] + count[i];
+	for (int i = 1; i < 10001; i++){
+		if (count[i] != count[i - 1]){
+			for (int j = 0; j < (count[i] - count[i - 1]); j++){
+				printf("%d\n", i);
+			}
+		}
 	}
-
-	for (int i = N; i >= 1; i--){
-		sortArr[countSum[input[i]]] = input[i];
-		countSum[input[i]]--;
-	}
-
-	for (int i = 1; i <= N; i++){
-		cout << sortArr[i] << endl;
-	}
-
 
 	return 0;
 }
