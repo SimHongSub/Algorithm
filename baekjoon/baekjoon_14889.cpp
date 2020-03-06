2ff2cedcb7382739dfc3ee73c53da4bcb08c4c2f
SimHongSub <oxhe2038@naver.com>
2020-03-06 19:11:48 +0900

    baekjoon algorithm 14889 incomplete

---
+++ baekjoon/baekjoon_14889.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+	int N;
+	int scores[101][101];
+	vector<int> subtraction_values;
+
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++){
+			scanf("%d", &scores[i][j]);
+		}
+	}
+
+	for (int i = 1; i < N / 2; i++){
+		
+	}
+
+
+	return 0;
+}
