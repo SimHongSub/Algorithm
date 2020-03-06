cad41c8f495377c1cf06ff85550569aedcfb2801
SimHongSub <oxhe2038@naver.com>
2020-03-06 18:20:02 +0900

    baekjoon algorithm 2407 answer

--- baekjoon/baekjoon_2407.cpp
+++ baekjoon/baekjoon_2407.cpp
@@ -1,136 +1,65 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector< vector< vector<int> > > combination_values;
-vector<int> combination(int n, int r);
+string cache[101][101];
 
-int main(){
+string bigNumAdd(string num1, string num2);
+string combination(int n, int r);
+
+int main(void){
 
 	int n, m;
-	vector<int> first;
-	vector<int> result;
 
-	first.push_back(1);
-
-	vector<vector<int>> vc;
-
-	vc.push_back(first);
-
-	combination_values.push_back(vc);
-	
 	scanf("%d %d", &n, &m);
 
-	result = combination(n, m);
-
-	for (int i = 0; i < result.size(); i++){
-		printf("%d", result[i]);
-	}
+	cout << combination(n, m) << endl;
 
 	return 0;
 }
 
-vector<int> combination(int n, int r){
-	if (n == 1 || n == r){
-		return combination_values[0][0];
-	}
+string bigNumAdd(string num1, string num2){
 
-	if (!combination_values[n][r].empty()){
-		return combination_values[n][r];
-	}else{
-		if (!combination_values[n - 1][r - 1].empty() && !combination_values[n - 1][r].empty()){
-			if (combination_values[n - 1][r - 1].size() > combination_values[n - 1][r].size()){
-				for (int i = 0; i < combination_values[n - 1][r - 1].size(); i++){
-					if (i <= combination_values[n - 1][r].size()){
-						if (combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i] >= 10){
-							int temp = combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i];
-							while (temp >= 10){
-								combination_values[n - 1][r - 1][i] += 1;
-								temp = temp - 10;
-							}
-							combination_values[n][r].insert(combination_values[n][r].begin(), temp);
-						}
-						else{
-							combination_values[n][r].insert(combination_values[n][r].begin(), combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i]);
-						}
-					}
-					else{
-						combination_values[n][r].insert(combination_values[n][r].begin(), combination_values[n - 1][r - 1][i]);
-					}
-				}
-			}
-			else{
-				for (int i = 0; i < combination_values[n - 1][r].size(); i++){
-					if (i <= combination_values[n - 1][r - 1].size()){
-						if (combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i] >= 10){
-							int temp = combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i];
-							while (temp >= 10){
-								combination_values[n - 1][r - 1][i] += 1;
-								temp = temp - 10;
-							}
-							combination_values[n][r].insert(combination_values[n][r].begin(), temp);
-						}
-						else{
-							combination_values[n][r].insert(combination_values[n][r].begin(), combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i]);
-						}
-					}
-					else{
-						combination_values[n][r].insert(combination_values[n][r].begin(), combination_values[n - 1][r][i]);
-					}
-				}
-			}
-		}else{
-			if (combination_values[n - 1][r - 1].empty()){
-				combination(n - 1, r - 1);
-			}
-			if (combination_values[n - 1][r].empty()){
-				combination(n - 1, r);
-			}
+	long long sum = 0;
+	string result;
 
-			if (combination_values[n - 1][r - 1].size() > combination_values[n - 1][r].size()){
-				for (int i = 0; i < combination_values[n - 1][r - 1].size(); i++){
-					if (i <= combination_values[n - 1][r].size()){
-						if (combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i] >= 10){
-							int temp = combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i];
-							while (temp >= 10){
-								combination_values[n - 1][r - 1][i] += 1;
-								temp = temp - 10;
-							}
-							combination_values[n][r].insert(combination_values[n][r].begin(), temp);
-						}
-						else{
-							combination_values[n][r].insert(combination_values[n][r].begin(), combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i]);
-						}
-					}
-					else{
-						combination_values[n][r].insert(combination_values[n][r].begin(), combination_values[n - 1][r - 1][i]);
-					}
-				}
-			}
-			else{
-				for (int i = 0; i < combination_values[n - 1][r].size(); i++){
-					if (i <= combination_values[n - 1][r - 1].size()){
-						if (combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i] >= 10){
-							int temp = combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i];
-							while (temp >= 10){
-								combination_values[n - 1][r - 1][i] += 1;
-								temp = temp - 10;
-							}
-							combination_values[n][r].insert(combination_values[n][r].begin(), temp);
-						}
-						else{
-							combination_values[n][r].insert(combination_values[n][r].begin(), combination_values[n - 1][r - 1][i] + combination_values[n - 1][r][i]);
-						}
-					}
-					else{
-						combination_values[n][r].insert(combination_values[n][r].begin(), combination_values[n - 1][r][i]);
-					}
-				}
-			}
+	while (!num1.empty() || !num2.empty() || sum){
+
+		if (!num1.empty()){
+			sum += num1.back() - '0';
+			num1.pop_back();
 		}
 
-		return combination_values[n][r];
+		if (!num2.empty()){
+			sum += num2.back() - '0';
+			num2.pop_back();
+		}
+
+		result.push_back((sum % 10) + '0');
+
+		sum /= 10;
 	}
+
+	reverse(result.begin(), result.end());
+
+	return result;
+}
+
+string combination(int n, int r){
+
+	if (n == r || r == 0){
+		return "1";
+	}
+
+	string &result = cache[n][r];
+
+	if (result != ""){
+		return result;
+	}
+
+	result = bigNumAdd(combination(n - 1, r - 1), combination(n - 1, r));
+
+	return result;
 }
