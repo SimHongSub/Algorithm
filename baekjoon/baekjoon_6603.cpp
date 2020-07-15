c3592fcf193c6b73ed229e540c1aad9ab0af233c
SimHongSub <oxhe2038@naver.com>
2020-07-15 19:23:44 +0900

    baekjoon algorithm 6603 proceeding

---
+++ baekjoon/baekjoon_6603.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+	int K;
+
+	scanf("%d", &K);
+
+	while (K != 0){
+
+		for (int i = 0; i < K; i++){
+			scanf("%d", )
+		}
+	}
+
+	return 0;
+}
