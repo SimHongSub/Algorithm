d66f0e423045574704939bc372fffefb77010110
SimHongSub <ghdtjq2038@gmail.com>
2021-02-08 13:36:13 +0900

    주석 추가

--- programmers/shared_taxi_fare.py
+++ programmers/shared_taxi_fare.py
@@ -7,24 +7,28 @@ def solution(n, s, a, b, fares):
     s -= 1
     a -= 1
     b -= 1
-    INF = math.inf
-    answer = math.inf
+    INF = math.inf   # 무한대 표현
+    answer = INF
     min_fee = [[INF for col in range(n)] for row in range(n)]
 
+    # 자기 자신으로 가는 비용 0으로 초기화
     for i in range(n):
         min_fee[i][i] = 0
 
+    # 주어진 비용으로 테이블 초기화
     for fare in fares:
         start, end, fee = fare
         min_fee[start - 1][end - 1] = fee
         min_fee[end - 1][start - 1] = fee
 
+    # 플로이드 워셜 알고리즘으로 모든 노드 최단 경로 구하기
     for k in range(n):
         for i in range(n):
             for j in range(n):
                 if min_fee[i][j] > min_fee[i][k] + min_fee[k][j]:
                     min_fee[i][j] = min_fee[i][k] + min_fee[k][j]
 
+    # 시작점(s)에서 각각의 도착지(a, b)에 도착하는데 드는 최소 비용 구하기
     for i in range(n):
         if answer > min_fee[s][i] + min_fee[i][a] + min_fee[i][b]:
             answer = min_fee[s][i] + min_fee[i][a] + min_fee[i][b]
