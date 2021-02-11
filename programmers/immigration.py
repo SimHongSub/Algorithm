e06a044c4b39989f32c0c34d7e3b26e43ee70d62
SimHongSub <ghdtjq2038@gmail.com>
2021-02-11 17:50:29 +0900

    이분탐색 : 입국심사

---
+++ programmers/immigration.py
@@ -0,0 +1,25 @@
+# 이분탐색 : 입국심사
+
+def solution(n, times):
+    answer = 0
+    left = 1
+    right = n * max(times)
+
+    while left <= right:
+        mid = (left + right) // 2
+        count = 0
+
+        for time in times:
+            count += mid // time
+
+            if count >= n:
+                break
+
+        if count >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return answer
+
