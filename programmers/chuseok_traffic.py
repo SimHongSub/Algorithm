b05aa532298c73a07a8460e5831c82a4b8dce748
SimHongSub <ghdtjq2038@gmail.com>
2021-03-05 10:29:16 +0900

    datetime 활용 : 추석 트래픽

--- programmers/chuseok_traffic.py
+++ programmers/chuseok_traffic.py
@@ -1,3 +1,13 @@
+from datetime import datetime, timedelta
+
+
+def set_times(process, time, times):
+    if time in times:
+        times[time].append(process)
+    else:
+        times[time] = [process]
+
+
 def solution(lines):
     answer = 0
     processes = [0 for i in range(len(lines))]
@@ -6,36 +16,36 @@ def solution(lines):
     for i in range(len(lines)):
         line = lines[i]
         date, complete_time, processing_time = line.split(" ")
+        date = date + " " + complete_time
 
-        processing_time = processing_time.replace("s", "")
-        h, m, s = complete_time.split(":")
+        if '.' in processing_time:
+            delay = processing_time.split('.')
+            delay[1] = delay[1][0:-1]
+        else:
+            delay = list(processing_time[0:-1])
+            delay += ["0"]
 
-        end_time = int(h) * 60 * 60 + int(m) * 60 + float(s)
-        start_time = end_time - float(processing_time) + 0.001 - 0.9991
+        end_time = datetime.fromisoformat(date)
+        start_time = end_time - timedelta(seconds=int(delay[0]) + 0.9991, milliseconds=int(delay[1]) - 1)
 
-        if start_time in times.keys():
-            start_time -= 0.00001
-
-        if end_time in times.keys():
-            end_time -= 0.00001
-
-        times[start_time] = i
-        times[end_time] = i
+        set_times(i, start_time, times)
+        set_times(i, end_time, times)
 
     for time in sorted(times.keys()):
         process = times[time]
 
-        if processes[process] == 0:
-            processes[process] = 1
-        else:
-            processes[process] = 0
+        for index in process:
+            if processes[index] == 0:
+                processes[index] = 1
+            else:
+                processes[index] = 0
 
-        if sum(processes) > answer:
-            answer = sum(processes)
+            if sum(processes) > answer:
+                answer = sum(processes)
 
     return answer
 
 
 if __name__ == "__main__":
-    solution(["2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"])
+    print(solution(["2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"]))
 
